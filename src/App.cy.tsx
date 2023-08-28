1f055a60ab96dd6e2bcb8aaf738b417c1da0d3d3
Lukas Harbarth <lukas.harbarth@sap.com>
2023-08-28 14:10:24 +0200

    use `getStaticAreaItemDomRef` in test

--- src/App.cy.tsx
+++ src/App.cy.tsx
@@ -1,14 +1,26 @@
 import App from "./App";
 
-describe("Component tests", () => {
+describe("Component tests", async () => {
   it("Test", () => {
     cy.mount(<App />);
     cy.get("[ui5-select]")
-        .as("select")
-        .contains("Option 2")
-        .should("not.be.visible");
+      .as("select")
+      .contains("Option 2")
+      .should("not.be.visible");
     cy.get("@select").click();
-    cy.get("[ui5-static-area-item]").contains("Option 2").click();
+
+    // leverage the `getStaticAreaItemDomRef` ui5-wc method, to access the static area item reference
+    cy.get("@select").then(async ($el) => {
+      const select = $el.get(0);
+      const staticAreaItemPromise = select
+        .getStaticAreaItemDomRef()
+        .then((data) => {
+          return data;
+        });
+      const staticAreaItem = await staticAreaItemPromise;
+      cy.wrap(staticAreaItem).as("staticAreaItem");
+    });
+    cy.get("@staticAreaItem").contains("Option 2").click();
     cy.get("@select").contains("Option 2").should("be.visible");
   });
 });

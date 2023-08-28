4888ad5d8d75ec07a8d4fb70cc0c700fa1545534
Lukas Harbarth <lukas.harbarth@sap.com>
2023-08-28 11:40:57 +0200

    add custom element demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,53 @@
 import { Select, Option, ThemeProvider } from "@ui5/webcomponents-react";
+import { useEffect, useRef, useState } from "react";
 
+// I disabled React strict mode to prevent hooks from running twice in dev mode
 
 function App() {
+  const selectRef = useRef(null);
+  const [isDefined, setIsDefined] = useState(false);
+
+  useEffect(() => {
+    console.log(
+      "initial",
+      // on initial render, the custom element of the web component won't be defined yet
+      customElements.get("ui5-select-demo"),
+      // because of that some attributes (like the unscoped tag-name) are not available
+      selectRef.current?.hasAttribute?.("ui5-select"),
+    );
+    // uncomment the debugger to see how the element looks in the dev tools at this point of time
+    // debugger;
+  }, []);
+
+  useEffect(() => {
+    const waitForDefine = async () => {
+      customElements.whenDefined("ui5-select-demo").then(() => {
+        setIsDefined(true);
+      });
+    };
+    if (!isDefined) {
+      waitForDefine();
+    } else {
+      console.log(
+        "wait for define",
+        // now the custom element is defined
+        customElements.get("ui5-select-demo"),
+        // and therefore the attribute is available
+        selectRef.current?.hasAttribute?.("ui5-select"),
+      );
+    }
+  }, [isDefined]);
+
   return (
-      <ThemeProvider>
-        <Select>
-          <Option>Option 1</Option>
-          <Option>Option 2</Option>
-          <Option>Option 3</Option>
-          <Option>Option 4</Option>
-          <Option>Option 5</Option>
-        </Select>
-      </ThemeProvider>
+    <ThemeProvider>
+      <Select ref={selectRef}>
+        <Option>Option 1</Option>
+        <Option>Option 2</Option>
+        <Option>Option 3</Option>
+        <Option>Option 4</Option>
+        <Option>Option 5</Option>
+      </Select>
+    </ThemeProvider>
   );
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
-import '@ui5/webcomponents-react/dist/Assets.js';
+import "@ui5/webcomponents-react/dist/Assets.js";
 import { setCustomElementsScopingSuffix } from "@ui5/webcomponents-base/dist/CustomElementsScope.js";
-setCustomElementsScopingSuffix("demo");
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root') as HTMLElement).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+setCustomElementsScopingSuffix("demo");
+
+createRoot(document.getElementById("root") as HTMLElement).render(
+  // <StrictMode>
+  <App />,
+  // </StrictMode>
 );
